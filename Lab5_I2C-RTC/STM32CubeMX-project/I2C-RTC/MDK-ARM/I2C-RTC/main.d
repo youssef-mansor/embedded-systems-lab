ad833a785e5d878d7b1d639fd4929a005b943153
youssef-mansour <youssef-mansour@aucegypt.edu>
2025-11-17 19:34:55 +0200

    alarm2 experiment

--- Lab5_I2C-RTC/STM32CubeMX-project/I2C-RTC/MDK-ARM/I2C-RTC/main.d
+++ Lab5_I2C-RTC/STM32CubeMX-project/I2C-RTC/MDK-ARM/I2C-RTC/main.d
@@ -33,4 +33,3 @@ i2c-rtc\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h
 i2c-rtc\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h
 i2c-rtc\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h
 i2c-rtc\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
-i2c-rtc\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
